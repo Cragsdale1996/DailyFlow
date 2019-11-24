564bb0247f28fb0d66b0cc835f3bdf898d2a4404
Cragsdale1996 <cragsdale@smu.edu>
2019-11-24 00:11:41 -0600

    session view ideation

--- src/App/pages/work-session-view/index.js
+++ src/App/pages/work-session-view/index.js
@@ -13,7 +13,18 @@ export default class WorkSessionView extends React.Component {
         <div className="work-session-view">
           <div className="row">
             <div className="col-md-12">
-              Henlo from Work Session View
+              Henlo from Work Session View (Task-to-session mapping). 
+              <br/><br/>
+              I should have a section that displays trello tasks (sorted by priority). This is my "Task Hub."
+              <br/><br/>
+              The main section should contain today's work session "tabs" with size directly relating to length of session.
+              Tasks should be able to be dragged and dropped directly into each session. Then, I should be able to configure 
+              the amount of time I want to dedicate to each task.
+              <br/><br/>
+              Work sessions should have default-able "routines" to choose from (for patterns of action). For example, "kindling the 
+              flame" should be automatically placed at the beginning of work sessions.
+              <br/><br/>
+              Tasks should also have reusable components that are selectable as steps. "Break into steps," "Get grasp on scope," etc...
             </div>
           </div>
         </div>
