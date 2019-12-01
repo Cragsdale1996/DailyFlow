afec46278510f504dca1e6d392f8c47d037cda0e
Cragsdale1996 <cragsdale@smu.edu>
2019-12-01 16:16:59 -0600

    initial API call dispatch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-//import 'babel-polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
